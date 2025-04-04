6ffdc01bfdb5bea6076db35bffb37885fde1fe38
BreinerFlorez <breinerferia10@gmail.com>
2025-04-04 18:20:38 -0500

    Update IngredientItemRepository.java

--- src/main/java/com/rest/Restaurante/repository/IngredientItemRepository.java
+++ src/main/java/com/rest/Restaurante/repository/IngredientItemRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface IngredientItemRepository extends JpaRepository<IngredientsItem, Long> {
-    List<IngredientsItem> findByRestaurant_Id(Long restaurantId);
+    List<IngredientsItem> findByRestaurantId(Long restaurantId);
 
 }
 
